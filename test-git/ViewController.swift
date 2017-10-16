6382d5951b580bd151be0754a8b4a3b46cb29ef3
git <git@LD140-13-06289.local>
2017-10-16 09:25:25 -0400

    Ajout méthode afficher et message "yeaaaaahhhh"

--- test-git/ViewController.swift
+++ test-git/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    func afficher(_ msg:String, fois:Int = 1) {
+        for i in 0...fois {
+            print(i, msg)
+        } // for
+    } // afficher
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("ViewController:viewDidLoad")
+        afficher("yeaaaaahhhh", fois:5)
     }
 
     override func didReceiveMemoryWarning() {
